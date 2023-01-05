19a8fb503746f8392e9b99dc88a083db30bac89a
niroshkumarh <64655849+niroshkumarh@users.noreply.github.com>
2023-01-05 15:51:00 +0530

    Update jquery.countdown.js

--- js/jquery.countdown.js
+++ js/jquery.countdown.js
@@ -34,7 +34,7 @@ and GPL-3.0 (http://opensource.org/licenses/GPL-3.0) licenses.
       };
       getDateData = function(endDate) {
         var dateData, diff;
-        endDate = Date.parse($.isPlainObject(_this.options.date) ? _this.options.date : new Date(2023,12,1));
+        endDate = Date.parse($.isPlainObject(_this.options.date) ? _this.options.date : new Date(2022,1,14));
         diff = (endDate - Date.parse(new Date)) / 1000;
         if (diff <= 0) {
           diff = 0;
